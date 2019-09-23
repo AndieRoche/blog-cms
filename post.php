2bde218a147d5512b61f01bef30c3a150194d83d
Andie Campos <andiecampos@Andies-MBP.attlocal.net>
2019-09-22 20:28:55 -0500

    Added Function to get Post Details

--- post.php
+++ post.php
@@ -6,12 +6,30 @@
 </head>
 
 <body>
-<?php include 'header.php' ?>
+<?php 
+	include 'header.php';
+
+	function getPostDetailsFromDatabase() { 
+			//TODO in Module 4
+			// Get Data from database instead
+			$postDetails = Array('title' => 'Post 1',
+								'content' => 'My First Blog',
+								'date' => '01/01/2019',
+								'author' => 'Andie R',
+								'content' => 'This is sample content.');
+			return $postDetails;
+	}
+	$postDetails = getPostDetailsFromDatabase();
+		
+
+?>
 	<div id="main">
-		<div id="author">Andie R</div>
-		<div id="date-published">Sept 10th, 2019</div>
-		<div id="content">
-		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+		<h1><?php echo $postDetails["title"]; ?></h1>
+		<div><?php echo $postDetails["author"]; ?></div>
+		<div><?php echo $postDetails["date"]; ?>
+		<p>
+		<?php echo $postDetails["content"]; ?>
+		</p>
 		</div>
 	
 	</div>
