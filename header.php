50bdfeb77774ca040b81390a406ea1f84cf76a03
Andie Campos <andiecampos@Andies-MBP.attlocal.net>
2019-09-22 19:52:00 -0500

    Added functions

--- header.php
+++ header.php
@@ -5,11 +5,25 @@
     $age = 35; //Integer type variable
     $married = true; //Boolean type variable
     $childrenNames = Array("Alice","Bob"); //Array type variable
-
+    
     $fullname = $firsname . " " . $lastname; //Concatenation Operator
     $dogYears = $age/7; //Arithmetic Operator
     $idAdult = $dogYears > 4; //Comparison Operator
     $isSettled = $isAdult and $married; //Integer Type Variable
+    
+    function getFullName($firstname, $lastname) { //Function Name
+        return $firstname . " " . $lastname; //Returned Valued
+    }
+    
+    $fullName = getFullName($firstname, $lastname); //Using Variable
+    $fullName = getFullName("Alice", "Doe"); //Using Values
+    
+    $function getIsSettled($age, $married) { //Function Name
+        $dogYears = $age/7; 
+        $idAdult = $dogYears > 4; 
+        $isSettled = $isAdult and $married; 
+        return $isSettled; //Returned Value
+    }
 ?>
 <div id="header">
 		<div id="nav">
