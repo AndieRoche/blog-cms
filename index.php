2e5ad3f9f13eb40ef35459fe4fc6692367fb5ec7
Andie Campos <andiecampos@Andies-MBP.attlocal.net>
2019-09-22 20:07:35 -0500

    Added Blog Post Function

--- index.php
+++ index.php
@@ -10,7 +10,19 @@
 	include 'header.php';
 ?>
 	<div id="main">
-	
+<?php 
+	$function getPostTitleFromDatabase() { 
+			//TODO in Module 4
+			// Get Data from database instead
+			$postTitles Array("Blog Post 1", "Blog Post 2", "Blog Post 3");
+			return $postTitles;
+	}
+	$postTitles = tPostTitlesFromDatabase();
+		
+		foreach($postTitles as $postTitle) {
+			echo "<li><a href='index.php'>" . $postTile . "</a></li>";
+	}
+?>	
 	</div>
 	<div id="footer">
 	
