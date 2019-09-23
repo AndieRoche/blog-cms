62823152f924be9515af0e7981fd0660d299541d
Andie Campos <andiecampos@Andies-MBP.attlocal.net>
2019-09-22 19:47:10 -0500

    Added Operators

--- header.php
+++ header.php
@@ -5,6 +5,11 @@
     $age = 35; //Integer type variable
     $married = true; //Boolean type variable
     $childrenNames = Array("Alice","Bob"); //Array type variable
+
+    $fullname = $firsname . " " . $lastname; //Concatenation Operator
+    $dogYears = $age/7; //Arithmetic Operator
+    $idAdult = $dogYears > 4; //Comparison Operator
+    $isSettled = $isAdult and $married; //Integer Type Variable
 ?>
 <div id="header">
 		<div id="nav">
