eecc14e3edf111a7ece6a1b1d57b2c2b01ec5a8a
Andie Campos <andiecampos@Andies-MBP.attlocal.net>
2019-09-29 21:38:23 -0500

    Get titles from database

--- index.php
+++ index.php
@@ -11,17 +11,25 @@
 ?>
 	<div id="main">
 <?php 
-	function getPostTitlesFromDatabase() { 
-			//TODO in Module 4
-			// Get Data from database instead
-			$postTitles = Array("Post 1","Post 2","Post 3");
-			return $postTitles;
+	function getPostTitlesFromDatabase() {
+		// Get the post title
+		$postTitle = rawurldecode($_GET["title"]);
+	
+		// Get the post that matches the postTitle
+		$sql = "SELECT * FROM blog_posts WHERE title='" . $postTitle . "'";
+		$result = mysqli_query($conn, $sql);
+	
+		// Get the first row from the result as an associative array
+		$postDetails = mysqli_fetch_assoc($result);
+		return $postDetails;
 	}
 	$postTitles = getPostTitlesFromDatabase();
 		
 	foreach($postTitles as $postTitle) {
 		echo "<li><a href='post.php?title=" . $postTitle . "'>" . $postTitle . "</a></li>";
 	}
+
+	
 ?>	
 	</div>
 	<div id="footer">
