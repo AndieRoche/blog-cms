9ec4c609382079a83159a4fe75c75f5464739c93
Andie Campos <andiecampos@Andies-MBP.attlocal.net>
2019-10-06 21:58:01 -0500

    added list.js

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 <meta charset="UTF-8">
 <title>Hello World</title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/list.js/1.5.0/list.min.js"></script>
 <script src="JS/blog.js"></script>
 </head>
 
