5214e833b5214a9c6e9f4bd56a77870998bed05d
Andie Campos <andiecampos@Andies-MBP.attlocal.net>
2019-10-06 21:04:57 -0500

    Added functions

--- hello.js
+++ hello.js
@@ -1,1 +1,22 @@
-alert("Hello World");
+var firstName = "Jane";                     // String type variable
+var lastName = "Doe";                       // String type variable
+var age = 35;                               // Number type variable
+var married = true;                         // Boolean type variable
+var childrenNames = ["Alice", "Bob"];       // Array type variable
+var husband = { firstName: "John", lastName: "Doe", age: 35 }     // Object type variable
+var fullName = firstName + " " + lastName;        // Concatenation operator
+
+function getFullName(firstName, lastName) {     // Function name
+    return  firstName + " " + lastName;         // Returned value
+}
+var fullName = getFullName(firstName, lastName);   // Using variables
+var fullName = getFullName("Alice", "Doe");        // Using values
+
+function getIsSettled(age, married) {     // Function name
+    var dogYears = age/7;                             // Arithmetic Operator
+    var isAdult = dogYears > 4;                       // Comparison operator
+    var isSettled = isAdult && married;               // Integer type variable
+    return  isSettled;         // Returned value
+}
+var isSettled = getIsSettled(age, married);   // Using variables
+var isSettled = getIsSettled(5, false);        // Using values
